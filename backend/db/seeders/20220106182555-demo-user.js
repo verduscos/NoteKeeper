d20b0a0e2584aaf9ac46b53e0051c79ca7744eb9
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-01-06 11:14:30 -0800

    completed user authentication for backend, tested and working

---
+++ backend/db/seeders/20220106182555-demo-user.js
@@ -0,0 +1,31 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      return queryInterface.bulkInsert('Users', [
+        {
+          email: 'demo@user.io',
+          username: 'Demo-lition',
+          hashedPassword: bcrypt.hashSync('password'),
+        },
+        {
+          email: 'demo2@user.io',
+          username: 'FakeUser2',
+          hashedPassword: bcrypt.hashSync('password2'),
+        },
+        {
+          email: 'demo3@user.io',
+          username: 'FakeUser3',
+          hashedPassword: bcrypt.hashSync('password3'),
+        },
+      ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser2', 'FakeUser3'] }
+    }, {});
+  }
+};
