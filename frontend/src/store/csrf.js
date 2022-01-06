4428495e82b2637f6b973ebfac71a5c71c3addd1
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-01-06 15:45:48 -0800

    csrfFetch func completed and tested

---
+++ frontend/src/store/csrf.js
@@ -0,0 +1,25 @@
+import Cookies from 'js-cookie';
+
+export async function csrfFetch(url, options={}) {
+  // set options.method to 'GET' if there is no method
+  options.method = options.method || 'GET';
+  // set options.headers to an empty object if there is no headers
+  options.headers = options.headers || {};
+
+  if (options.method.toUpperCase() !== 'GET') {
+      options.headers['Content-Type'] =
+        options.headers['Content-Type'] || 'aplication/json';
+      options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
+  }
+
+  const res = await window.fetch(url, options);
+
+  if (res.state >= 400) throw res;
+
+  return res;
+}
+
+// call this to get the "XSRF-TOKEN" cookie, should only be used in development
+export function restoreCSRF() {
+  return csrfFetch('/api/csrf/restore');
+}
