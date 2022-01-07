76ed0613eb2362318256c370800532653aa24696
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-01-07 07:45:56 -0800

    login form working correctly

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -8,7 +8,7 @@ export async function csrfFetch(url, options={}) {
 
   if (options.method.toUpperCase() !== 'GET') {
       options.headers['Content-Type'] =
-        options.headers['Content-Type'] || 'aplication/json';
+        options.headers['Content-Type'] || 'application/json';
       options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
   }
 
