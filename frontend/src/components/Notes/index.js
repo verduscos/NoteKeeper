6fbe8ecbbec0afe53789c03d410c9392e358328e
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-01-10 13:55:53 -0800

    able to render note info in notes component

--- frontend/src/components/Notes/index.js
+++ frontend/src/components/Notes/index.js
@@ -5,18 +5,33 @@ import { useDispatch, useSelector } from 'react-redux';
 
 function Notes() {
     const dispatch = useDispatch()
+    // const [loaded, setLoaded] = useState();
     const [notes, setNotes] = useState=([])
 
-    const userNotes = useSelector(state => state.notes);
+    const userNotes = useSelector(state => state.notes.notes);
+    console.log(userNotes)
 
     useEffect(() => {
         dispatch(sessionActions.getNotesThunk())
+        //adding loaded is not chaning anything atm, fixed loading issue with '?'
+
     }, [dispatch])
 
 
-    return (
+    // if (loaded) {
+
+        return (
+        <>
         <h1>Notes</h1>
+        {userNotes?.map(note => (
+            <>
+            <div>{note.title}</div>
+            <div>{note.body}</div>
+            </>
+        ))}
+        </>
     )
+// }
 }
 
 export default Notes;
