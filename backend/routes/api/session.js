e356ff2c9726127525698460e11d26b62c3fac4a
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-01-09 16:34:05 -0800

    created demo route, thunk, form

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -70,5 +70,32 @@ router.get(
 )
 
 
+// Demo user
+router.post (
+    '/demo',
+    validateLogin,
+    asyncHandler(async (req, res, next) => {
+        const credential = 'Demo-lition';
+        const password = 'password';
+
+        const user =  await User.login({ credential, password});
+
+        if (!user) {
+            const err = new Error('Login failed');
+            err.status = 401;
+            err.title = 'Login failed';
+            err.errors = ['The provided credentials were invalid.'];
+            console.log(err.errors)
+            return next(err);
+        }
+
+        await setTokenCookie(res, user);
+
+        return res.json({
+            user,
+        });
+    }),
+);
+
 
 module.exports = router;
