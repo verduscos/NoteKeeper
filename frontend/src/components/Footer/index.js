0e123113cc1f5f402e2713ad717ed2e56ace1f16
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-01-13 11:26:51 -0800

    added footer links

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -6,7 +6,11 @@ function Footer() {
     return (
         <div id='footer-container'>
             {/* <h1>salfjdaskl</h1> */}
-            <p>Created by Eddie Verdusco</p>
+            <div id='footer-links'>
+                <p>Created by Eddie Verdusco</p>
+                <a className='footer-link'  href='https://github.com/verduscos' target="_blank" ><i class="fab fa-github-square"></i></a>
+                <a className='footer-link' href='https://www.linkedin.com/in/eddie-verdusco/' target='_blank'><i class="fab fa-linkedin"></i></a>
+            </div>
             <p>JavaScript | React | Redux | Node.js | Express.js | HTML | CSS</p>
         </div>
     )
--- frontend/src/components/Notes/currentNote.js
+++ frontend/src/components/Notes/currentNote.js
@@ -1,7 +1,7 @@
 import React, {  useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/notes';
-import { useParams, Redirect, useHistory, Link} from 'react-router-dom';
+import { useParams, useHistory, Link} from 'react-router-dom';
 import './CurrentNote.css';
 
 
@@ -12,83 +12,50 @@ function CurrentNote() {
     const dispatch = useDispatch();
     const currentUser = useSelector(state => state.session.user);
     const userNotes = useSelector(state => state.notes.notes)
-
     const  { noteId } = params;
-
-
-
     const [currtitle, setcurrTitle] = useState('');
     const [currentNote, setCurrentNote] = useState('');
     const [errors, setErrors] = useState([]);
 
-// const checkErrors = (title, body) => {
-//     const errors = [];
-//     if (title.length < 4) errors.push('title errors');
-//     if (!body.length) errors.push('body errors');
 
-//     setErrors(errors)
+    // EDIT
+    const handleEdit = async (e) => {
+        e.preventDefault();
 
-//     return errors;
-// }
-
-
-        // EDIT
-        const handleEdit = async (e) => {
-            e.preventDefault();
-
-            const payload = {
-                id: noteId,
-                title: currtitle,
-                body: currentNote,
-                userId: currentUser.id
-            }
-
-            setErrors([]);
-
-            // if (currtitle.length > 4) {
-            //     return checkErrors(currtitle, currentNote)
-            // } else {
-
-
-            return dispatch(sessionActions.editNoteThunk(payload, noteId))
-            .catch(async (res) => {
-                const data = await res.json();
-                if (data && data.errors) setErrors(data.errors);
-            })
-        // }
-            // return
+        const payload = {
+            id: noteId,
+            title: currtitle,
+            body: currentNote,
+            userId: currentUser.id
         }
 
+        setErrors([]);
+
+        return dispatch(sessionActions.editNoteThunk(payload, noteId))
+        .catch(async (res) => {
+            const data = await res.json();
+            if (data && data.errors) setErrors(data.errors);
+        })
+    }
+
     // DELETE
-    // const handleDelete = async (e, id) => {
-    //     e.preventDefault();
-
-    //     dispatch(sessionActions.removeNoteThunk(id))
-    //     dispatch(sessionActions.getNotesThunk(currentUser?.id))
-    //     return
-    // }
-
     const handleDelete = async (e, id) => {
         e.preventDefault();
 
         const res = await dispatch(sessionActions.removeNoteThunk(id))
-        // await dispatch(sessionActions.getNotesThunk(currentUser?.id))
         if(res.id) history.push('/mynotes/notes')
-
-
         return
     }
 
     useEffect(() => {
         dispatch(sessionActions.getNotesThunk(currentUser?.id))
-        // dispatch(sessionActions.removeNoteThunk(noteId))
 
         const  { title, body } = userNotes?.find(note =>
             note.id === +noteId
-            )
+        )
 
-            setcurrTitle(title)
-setCurrentNote(body)
+        setcurrTitle(title)
+        setCurrentNote(body)
     }, [dispatch, noteId])
 
 
