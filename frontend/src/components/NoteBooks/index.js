e7e98997eb5f66d62be0dd7bcec7ffcdb3e23ac6
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-03-16 16:55:22 -0700

    updating css

--- frontend/src/components/NoteBooks/index.js
+++ frontend/src/components/NoteBooks/index.js
@@ -7,7 +7,8 @@ import ProfileButton from '../Navigation/ProfileButton';
 import { postNotebookThunk, deleteNotebookThunk, updateNotebookThunk } from '../../store/notebooks';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
-import { IoIosArrowDown } from 'react-icons/io'
+import { IoIosArrowDown } from 'react-icons/io';
+import { BiDotsVerticalRounded } from 'react-icons/bi';
 import './Notebooks.css';
 
 function Notebooks() {
@@ -20,6 +21,7 @@ function Notebooks() {
   const [notebookName, setNotebookName] = useState("");
   const [highlight, setHighlight] = useState("highlight-notebook")
   const [errors, setErrors] = useState("");
+  const [showEditNotebook, setShowEditNotebook] = useState(false)
 
   const getNotebookNotes = (e, notebookId) => {
     e.preventDefault();
@@ -58,9 +60,9 @@ function Notebooks() {
 
       <CreateNotebook user={currentUser} />
 
-      <div id='note-container'>
+      <div>
         {notebooks?.map(notebook => (
-          <div id="notebook-container" className={ notebookId == notebook?.id ? highlight : ""}>
+          <div id="notebook-container" className={notebookId == notebook?.id ? highlight : ""} >
             <Link
               to={`/mynotes/notebook/${notebook?.id}`}
               // onClick={(e) => {
@@ -69,22 +71,34 @@ function Notebooks() {
               id='notebook-links'>{notebook?.title}</Link>
 
 
-              { notebookId == notebook?.id ? <h1>test</h1> : null}
+            {notebookId == notebook?.id ? <BiDotsVerticalRounded
+            onClick={(e) => {
+              e.preventDefault();
+              setShowEditNotebook(!showEditNotebook);
+            }} id="notebook-edit-btn" /> : null}
 
-            <span onClick={(e) => {
-              deleteNotebook(e, notebook.id)
-            }} className="delete">X</span>
 
-            <form onSubmit={(e) => {
-              updateNotebook(e, notebook.id)
-            }}>
-              <input value={notebookName} onChange={(e) => {
-                e.preventDefault()
-                setNotebookName(e.target.value)
-              }} type="text" />
+            {showEditNotebook && notebookId == notebook?.id ?
+              <div id="notebook-edit-container">
+                <form onSubmit={(e) => {
+                  updateNotebook(e, notebook.id)
+                }}>
+                  <input value={notebookName} onChange={(e) => {
+                    e.preventDefault()
+                    setNotebookName(e.target.value)
+                  }} type="text" placeholder="Edit notebook title..." id="edit-notebook-input" />
 
-              <button className="delete">O</button>
-            </form>
+                  {/* <button className="delete">Edit</button> */}
+                </form>
+
+                <span onClick={(e) => {
+                  deleteNotebook(e, notebook.id)
+                }} className="delete">Delete notebook</span>
+              </div>
+
+
+
+              : null}
 
           </div>
         ))}
