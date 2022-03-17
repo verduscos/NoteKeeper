bd918b736c81b25b2814ff7aab0fe74e6382a4d1
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-03-17 09:30:33 -0700

    updated css for notes column

--- frontend/src/components/Notes/index.js
+++ frontend/src/components/Notes/index.js
@@ -8,156 +8,167 @@ import { Link, Route } from 'react-router-dom';
 import ProfileButton from '../Navigation/ProfileButton';
 import './NoteList.css'
 import Create from './Create';
+import { MdStickyNote2 } from "react-icons/md"
 
 function Notes() {
-    const currentUser = useSelector(state => state.session.user);
-    const dispatch = useDispatch()
-    // const [loaded, setLoaded] = useState();
-    // const [notes, setNotes] = useState=([])
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
-    const [userId, setUserId] = useState(null);
-    const [notebookId, setNotebookId] = useState(null);
-    const [displayNote, setDisplayNote] = useState('')
-    const [errors, setErrors] = useState([]);
-    const [created, setCreated] = useState(false);
+  const currentUser = useSelector(state => state.session.user);
+  const dispatch = useDispatch()
+  // const [loaded, setLoaded] = useState();
+  // const [notes, setNotes] = useState=([])
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [userId, setUserId] = useState(null);
+  const [notebookId, setNotebookId] = useState(null);
+  const [displayNote, setDisplayNote] = useState('')
+  const [errors, setErrors] = useState([]);
+  const [created, setCreated] = useState(false);
 
-    let notifcation;
-    if (created) {
-        notifcation = (
-            <div className='notification'>
-                <p>Note Created! </p> <i id='notification-check' className="fas fa-check-square"></i>
-            </div>
-        )
-    } else {
-        notifcation = (
-            null
-        )
+  let notifcation;
+  if (created) {
+    notifcation = (
+      <div className='notification'>
+        <p>Note Created! </p> <i id='notification-check' className="fas fa-check-square"></i>
+      </div>
+    )
+  } else {
+    notifcation = (
+      null
+    )
+  }
+
+
+  const handleErrors = (e) => {
+    e.preventDefault();
+
+    const payload = {
+      title,
+      body,
+      userId: currentUser.id,
+      notebookId
     }
+    setErrors([]);
+
+    if (title.length >= 4 && body.length >= 1) setCreated(true)
+
+    return dispatch(sessionActions.createNoteThunk(payload))
+      .catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors)
+
+      })
+  }
 
 
-    const handleErrors = (e) => {
-       e.preventDefault();
 
-       const payload = {
-           title,
-           body,
-           userId: currentUser.id,
-           notebookId
-        }
-        setErrors([]);
+  if (created) {
+    setTimeout(() => {
+      setCreated(false);
+    }, 2000)
+  }
 
-        if (title.length >= 4 && body.length >= 1) setCreated(true)
+  const userNotes = useSelector(state => state.notes);
+  let notes = Object.values(userNotes)
 
-            return dispatch(sessionActions.createNoteThunk(payload))
-            .catch(async (res) => {
-                const data = await res.json();
-                if (data && data.errors) setErrors(data.errors)
 
-            })
+  const handleCreate = (e) => {
+    e.preventDefault();
+
+    const payload = {
+      title,
+      body,
+      userId: currentUser.id,
+      notebookId
     }
+    dispatch(sessionActions.createNoteThunk(payload))
 
+    return
+  }
 
-
-    if (created) {
-        setTimeout(() => {
-            setCreated(false);
-        }, 2000)
-    }
-
-    const userNotes = useSelector(state => state.notes);
-    let notes = Object.values(userNotes)
-
-    const handleCreate = (e) => {
-        e.preventDefault();
-
-        const payload = {
-                title,
-                body,
-                userId: currentUser.id,
-                notebookId
-              }
-         dispatch(sessionActions.createNoteThunk(payload))
-
-        return
-    }
-
-    const getNote = (e, noteId) => {
-      e.preventDefault();
-      dispatch(getSingleNoteThunk(currentUser.id, noteId))
-      // dispatch(sessionActions.getSingleNoteThunk(currentUser.id, noteId));
-    }
+  const getNote = (e, noteId) => {
+    e.preventDefault();
+    dispatch(getSingleNoteThunk(currentUser.id, noteId))
+    // dispatch(sessionActions.getSingleNoteThunk(currentUser.id, noteId));
+  }
 
 
 
 
-    useEffect(() => {
-        // dispatch(sessionActions.getNotesThunk(currentUser.id))
-        dispatch(notebookActions.getNootbooksThunk(currentUser.id))
-        //adding loaded is not chaning anything atm, fixed loading issue with '?'
+  useEffect(() => {
+    // dispatch(sessionActions.getNotesThunk(currentUser.id))
+    dispatch(notebookActions.getNootbooksThunk(currentUser.id))
+    //adding loaded is not chaning anything atm, fixed loading issue with '?'
 
-    }, [dispatch])
-    //rerender when deleting a note
+  }, [dispatch])
+  //rerender when deleting a note
 
 
-    // if (loaded) {
+  // if (loaded) {
 
-        return (
+  return (
+    <>
+      {notes[0] !== null ?
         <div id='bg'>
-           {notifcation}
-        <div id='note-link-column'>
-            <h1>notes</h1>
+          {notifcation}
+          <div id='note-link-column'>
+            <div id="notes-header-container">
+              <MdStickyNote2 />
+              <h1 id="notes-header">Notes</h1>
+            </div>
+            <p id="notes-length">{notes?.length} notes</p>
 
             <div id='note-container'>
-            {notes?.map(note => (
+              {notes?.map(note => (
                 <div onClick={(e) => {
                   getNote(e, note?.id)
                 }}
-                key={note?.id}
+                  key={note?.id} id="note-containers"
                 >
-                    <Link key={`${note?.id}-link`}id='note-links'  to={`/mynotes/notebook/${note?.notebookId}/notes/${note?.id}`}>{note?.title}</Link>
+                  <Link key={`${note?.id}-link`} id='note-links' to={`/mynotes/notebook/${note?.notebookId}/notes/${note?.id}`}>{note?.title}</Link>
+                  {/* <p>{note?.createdAt.split('T')[0].split('-')[1]}/{note?.createdAt.split('T')[0].split('-')[2]}/{note?.createdAt.split('T')[0].split('-')[0]}</p> */}
                 </div>
-            ))}
+              ))}
 
-        </div>
+            </div>
 
-        {/* <Route exact path='/mynotes/notes/:noteId'>
+            {/* <Route exact path='/mynotes/notes/:noteId'>
             <CurrentNote />
-        </Route> */}
-        {/* <Route exact path='/mynotes/notes/new'>
+          </Route> */}
+            {/* <Route exact path='/mynotes/notes/new'>
             <Create />
-        </Route> */}
+          </Route> */}
 
 
             {/* <form onSubmit={handleErrors} id='create-form'>
                 <div className='title-container'>
-                    <h2>Create a new note</h2>
+                <h2>Create a new note</h2>
 
                 <input  onChange={(e) => {
-                setTitle(e.target.value);
-            }}
-            required
-            id='createNote-title'
-            type='text' value={title} placeholder='Title'/>
+                  setTitle(e.target.value);
+                }}
+                required
+                id='createNote-title'
+                type='text' value={title} placeholder='Title'/>
                 </div>
 
                 <ul className='errors'>
-                    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
                 </ul>
 
-            <textarea onChange={(e) => {
-                setBody(e.target.value);
-            }}
-            className='displayNote'
-            type='texarea' value={body} placeholder='Start writing...'/>
-            <button
-            className='butts'
-            >Create</button>
-        </form> */}
-        {/* <Link to='/mynotes/notes'>Create</Link> */}
+                <textarea onChange={(e) => {
+                  setBody(e.target.value);
+                }}
+                className='displayNote'
+                type='texarea' value={body} placeholder='Start writing...'/>
+                <button
+                className='butts'
+                >Create</button>
+              </form> */}
+            {/* <Link to='/mynotes/notes'>Create</Link> */}
+          </div>
         </div>
-        </div>
-    )
+        : null}
+    </>
+  )
 }
 
 export default Notes;
