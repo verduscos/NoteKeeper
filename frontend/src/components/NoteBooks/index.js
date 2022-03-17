d628852968036a9a32f2f624686b41ff47e4382f
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-03-17 12:34:51 -0700

    updating usestate vars for notebook edit form

--- frontend/src/components/NoteBooks/index.js
+++ frontend/src/components/NoteBooks/index.js
@@ -9,6 +9,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { IoIosArrowDown } from 'react-icons/io';
 import { BiDotsVerticalRounded } from 'react-icons/bi';
+import { RiSave3Fill, RiDeleteBin5Fill } from 'react-icons/ri'
+import { AiFillCloseSquare } from 'react-icons/ai'
 import './Notebooks.css';
 
 function Notebooks() {
@@ -28,6 +30,10 @@ function Notebooks() {
     dispatch(getNotebookNotesThunk(currentUser.id, notebookId));
   }
 
+  const remove = () => {
+    setShowEditNotebook(false);
+  }
+
 
   const deleteNotebook = (e, id) => {
     e.preventDefault();
@@ -45,7 +51,14 @@ function Notebooks() {
       title: notebookName
     }
 
-    dispatch(updateNotebookThunk(payload))
+    if (notebookName.length >= 4) {
+      dispatch(updateNotebookThunk(payload));
+      setShowEditNotebook(false);
+      setErrors("");
+    } else {
+      setErrors("Title must be at least 4 characters.")
+    }
+
   }
 
   useEffect(() => {
@@ -72,28 +85,46 @@ function Notebooks() {
 
 
             {notebookId == notebook?.id ? <BiDotsVerticalRounded
-            onClick={(e) => {
-              e.preventDefault();
-              setShowEditNotebook(!showEditNotebook);
-            }} id="notebook-edit-btn" /> : null}
+              onClick={(e) => {
+                e.preventDefault();
+                setShowEditNotebook(!showEditNotebook);
+              }} id="notebook-edit-btn" /> : null}
 
 
             {showEditNotebook && notebookId == notebook?.id ?
-              <div id="notebook-edit-container">
+              <div id="notebook-edit-container"
+                onClick={(e) => {
+                  e.preventDefault();
+                  // setShowEditNotebook(!showEditNotebook);
+                }}
+              // onBlur={(e) => {
+              //   e.preventDefault();
+              //   setShowEditNotebook(false);
+              // }}
+              >
+                {errors.length ?
+                  <p className="errors-modal">*{errors}</p>
+                  : null}
                 <form onSubmit={(e) => {
                   updateNotebook(e, notebook.id)
-                }}>
+                }} id="notebook-edit-form">
+
+                  {/* <p>Update notebook title</p> */}
                   <input value={notebookName} onChange={(e) => {
                     e.preventDefault()
                     setNotebookName(e.target.value)
                   }} type="text" placeholder="Edit notebook title..." id="edit-notebook-input" />
 
-                  {/* <button className="delete">Edit</button> */}
+                  <div id="edit-notebook-btn" onClick={(e) => {
+                    e.preventDefault();
+                    updateNotebook(e, notebook.id)
+                  }}><RiSave3Fill /></div>
                 </form>
 
-                <span onClick={(e) => {
+                <button onClick={(e) => {
                   deleteNotebook(e, notebook.id)
-                }} className="delete">Delete notebook</span>
+                }} id="delete-notebook-btn"><RiDeleteBin5Fill /></button>
+                {/* <div onClick={remove} id="delete-notebook-btn"><AiFillCloseSquare /></div> */}
               </div>
 
 
